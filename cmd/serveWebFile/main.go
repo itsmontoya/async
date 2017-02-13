8255465bb54769b42f024aa58e5950f7c5a5435a
itsMontoya <josh@montoya.io>
2017-02-13 11:18:05 -0800

    Adding copy benches to serveWebFile

--- cmd/serveWebFile/main.go
+++ cmd/serveWebFile/main.go
@@ -5,7 +5,10 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/itsmontoya/async"
 	"github.com/itsmontoya/async/file"
+	aio "github.com/itsmontoya/async/io"
+
 	"github.com/julienschmidt/httprouter"
 	"github.com/valyala/fasthttp"
 )
@@ -14,16 +17,18 @@ const fileLoc = "../../testing/declarationOfIndependence.txt"
 
 func main() {
 	s := &srv{}
+	async.Set(2)
 
 	go func() {
 		r := httprouter.New()
 		r.GET("/a", s.handleA)
 		r.GET("/b", s.handleB)
+		r.GET("/c", s.handleC)
 		http.ListenAndServe(":1337", r)
 	}()
 
 	go func() {
-		fasthttp.ListenAndServe(":8081", s.handleC)
+		fasthttp.ListenAndServe(":8081", s.handleD)
 	}()
 
 	select {}
@@ -62,7 +67,22 @@ func (s *srv) handleB(w http.ResponseWriter, r *http.Request, p httprouter.Param
 	}
 }
 
-func (s *srv) handleC(ctx *fasthttp.RequestCtx) {
+func (s *srv) handleC(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	f, err := os.Open(fileLoc)
+	if err != nil {
+		return
+	}
+
+	if _, err = aio.Copy(w, f); err != nil {
+		return
+	}
+
+	if err = f.Close(); err != nil {
+		return
+	}
+}
+
+func (s *srv) handleD(ctx *fasthttp.RequestCtx) {
 	f, err := file.Open(fileLoc)
 	if err != nil {
 		return
